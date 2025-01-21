ea004511e08b95f80c79d7620221233b2143f040
Giorgi Meskhidze <Giorgi.meskh@gmail.com>
2025-01-21 09:28:37 -0500

    Initialize cd/feature/add-sc-address-for-all-envs

---
+++ src/config/config.mainnet.ts
@@ -0,0 +1,9 @@
+import { EnvironmentsEnum } from 'types';
+
+export * from './sharedConfig';
+
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgq6wvtphapx0xju6mgk65aryzecat4fcry9l5qycup8h';
+export const API_URL = 'https://template-api.multiversx.com';
+export const sampleAuthenticatedDomains = [API_URL];
+export const environment = EnvironmentsEnum.mainnet;
---
+++ src/config/config.testnet.ts
@@ -0,0 +1,9 @@
+import { EnvironmentsEnum } from 'types';
+
+export * from './sharedConfig';
+
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgq4rnqmppypqdy263c9qjsy8uwurts47uv9l5q028464';
+export const API_URL = 'https://testnet-template-api.multiversx.com';
+export const sampleAuthenticatedDomains = [API_URL];
+export const environment = EnvironmentsEnum.testnet;
